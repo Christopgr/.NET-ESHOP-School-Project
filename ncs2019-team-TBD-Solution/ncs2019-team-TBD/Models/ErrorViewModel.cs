9c87e11f59adbea3abd3e07a2e965c94b353bb24
George Tsakountakis <tsakountakis@gmail.com>
2019-04-04 19:06:24 +0300

    first commit / web app files

---
+++ ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ncs2019_team_TBD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
