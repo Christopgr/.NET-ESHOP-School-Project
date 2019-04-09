b1bd256e52f64b5704865425fe2e4fc84c71c70e
Chris Topalis <chrisstop@hotmail.com>
2019-04-09 18:54:45 +0300

    Product Table (Class)

---
+++ ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ncs2019_team_TBD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int InventoryQuantity { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    
+        public int SerialNumber { get; set; }
+
+        public int Price { get; set; }
+    }
+}
