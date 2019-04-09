82ee304952154cfbab31c474d4bb19effc3ce00d
Chris Topalis <chrisstop@hotmail.com>
2019-04-09 19:05:20 +0300

    Changes on ApplicationDbContext.cs for fuctional tables.

--- ncs2019-team-TBD-Solution/ncs2019-team-TBD/Data/ApplicationDbContext.cs
+++ ncs2019-team-TBD-Solution/ncs2019-team-TBD/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ncs2019_team_TBD.Models;
 
 namespace ncs2019_team_TBD.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
