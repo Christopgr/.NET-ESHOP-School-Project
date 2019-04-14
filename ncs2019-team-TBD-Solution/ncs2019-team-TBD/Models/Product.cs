d9163311b484b8053fe0fd92210c079222e21f48
George Tsakountakis <tsakountakis@gmail.com>
2019-04-14 14:13:15 +0300

    Model change (public double Price)

--- ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/Product.cs
+++ ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/Product.cs
@@ -16,7 +16,7 @@ namespace ncs2019_team_TBD.Models
     
         public int SerialNumber { get; set; }
 
-        public int Price { get; set; }
+        public double Price { get; set; }
 
 		/// <summary>
 		/// only 1 Category
