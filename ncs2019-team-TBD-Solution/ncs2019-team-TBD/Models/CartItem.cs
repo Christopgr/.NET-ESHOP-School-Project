de9e0ddee22d5864c142b3826ded89ed9e5a3d38
Chris Topalis <chrisstop@hotmail.com>
2019-05-01 20:19:49 +0300

    Removes virtual Cart cart from CartItem model

--- ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/CartItem.cs
+++ ncs2019-team-TBD-Solution/ncs2019-team-TBD/Models/CartItem.cs
@@ -9,8 +9,6 @@ namespace ncs2019_team_TBD.Models
 	{
 		public int CartId { get; set; }
 
-        public virtual Cart Cart { get; set; }
-
         public int ProductId { get; set; }
 
 		public int Quantity { get; set; }
